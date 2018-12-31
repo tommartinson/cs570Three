a20cf1161f8a7fc9ba773247f6d65ca0510741d8
Tom Martinson <38024425+tommartinson@users.noreply.github.com>
2018-12-31 12:40:11 -0800

    Add files via upload

---
+++ Three/p3helper.c
@@ -0,0 +1,35 @@
+/* p3helper.c
+ * <YOUR NAME HERE>
+ * Program 3
+ * CS570
+ * <INSTRUCTOR NAME HERE>
+ * SDSU
+ * <DATE HERE>
+ *
+ * This is the only file you may change. (In fact, the other files should
+ * be symbolic links to:
+ *   ~cs570/Three/p3main.c
+ *   ~cs570/Three/p3.h
+ *   ~cs570/Three/Makefile
+ *   ~cs570/Three/CHK.h    )
+ *
+ */
+#include "p3.h"
+
+/* You may put any semaphore (or other global) declarations/definitions here: */
+
+
+
+
+/* General documentation for the following functions is in p3.h
+ * Here you supply the code:
+ */
+void initstudentstuff(void) {
+}
+
+void prolog(int kind) {
+}
+
+void epilog(int kind) {
+}  
+
